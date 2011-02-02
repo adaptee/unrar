ca08ece3972a58bc0b4077ba7911b9b979905bc5
Adaptee <adaptee@gmail.com>
2011-02-02 21:23:26 +0800

    remove non-unix code

--- extinfo.cpp
+++ extinfo.cpp
@@ -1,15 +1,5 @@
 #include "rar.hpp"
 
-#ifdef _WIN_ALL
-#include "win32acl.cpp"
-#include "win32stm.cpp"
-#endif
-#ifdef _BEOS
-#include "beosea.cpp"
-#endif
-#if defined(_EMX) && !defined(_DJGPP)
-#include "os2ea.cpp"
-#endif
 #ifdef _UNIX
 #include "uowners.cpp"
 #endif
@@ -17,35 +7,35 @@
 
 
 #ifndef SFX_MODULE
-void SetExtraInfo(CommandData *Cmd,Archive &Arc,char *Name,wchar *NameW)
+void SetExtraInfo(CommandData *Cmd, Archive &Arc, char *Name, wchar *NameW)
 {
   switch(Arc.SubBlockHead.SubType)
   {
 #if defined(_EMX) && !defined(_DJGPP)
     case EA_HEAD:
       if (Cmd->ProcessEA)
-        ExtractOS2EA(Arc,Name);
+        ExtractOS2EA(Arc, Name);
       break;
 #endif
 #ifdef _UNIX
     case UO_HEAD:
       if (Cmd->ProcessOwners)
-        ExtractUnixOwner(Arc,Name);
+        ExtractUnixOwner(Arc, Name);
       break;
 #endif
 #ifdef _BEOS
     case BEEA_HEAD:
       if (Cmd->ProcessEA)
-        ExtractBeEA(Arc,Name);
+        ExtractBeEA(Arc, Name);
       break;
 #endif
 #ifdef _WIN_ALL
     case NTACL_HEAD:
       if (Cmd->ProcessOwners)
-        ExtractACL(Arc,Name,NameW);
+        ExtractACL(Arc, Name, NameW);
       break;
     case STREAM_HEAD:
-      ExtractStreams(Arc,Name,NameW);
+      ExtractStreams(Arc, Name, NameW);
       break;
 #endif
   }
@@ -53,24 +43,24 @@ void SetExtraInfo(CommandData *Cmd,Archive &Arc,char *Name,wchar *NameW)
 #endif
 
 
-void SetExtraInfoNew(CommandData *Cmd,Archive &Arc,char *Name,wchar *NameW)
+void SetExtraInfoNew(CommandData *Cmd, Archive &Arc, char *Name, wchar *NameW)
 {
 #if defined(_EMX) && !defined(_DJGPP)
   if (Cmd->ProcessEA && Arc.SubHead.CmpName(SUBHEAD_TYPE_OS2EA))
-    ExtractOS2EANew(Arc,Name);
+    ExtractOS2EANew(Arc, Name);
 #endif
 #ifdef _UNIX
   if (Cmd->ProcessOwners && Arc.SubHead.CmpName(SUBHEAD_TYPE_UOWNER))
-    ExtractUnixOwnerNew(Arc,Name);
+    ExtractUnixOwnerNew(Arc, Name);
 #endif
 #ifdef _BEOS
   if (Cmd->ProcessEA && Arc.SubHead.CmpName(SUBHEAD_TYPE_UOWNER))
-    ExtractUnixOwnerNew(Arc,Name);
+    ExtractUnixOwnerNew(Arc, Name);
 #endif
 #ifdef _WIN_ALL
   if (Cmd->ProcessOwners && Arc.SubHead.CmpName(SUBHEAD_TYPE_ACL))
-    ExtractACLNew(Arc,Name,NameW);
+    ExtractACLNew(Arc, Name, NameW);
   if (Arc.SubHead.CmpName(SUBHEAD_TYPE_STREAM))
-    ExtractStreamsNew(Arc,Name,NameW);
+    ExtractStreamsNew(Arc, Name, NameW);
 #endif
 }
