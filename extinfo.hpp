d17cb1cfc1475b71840794d83eecd87832c0d87f
Adaptee <adaptee@gmail.com>
2011-02-03 11:37:36 +0800

    make extinfo.hpp stand alone

--- extinfo.hpp
+++ extinfo.hpp
@@ -1,8 +1,12 @@
 #ifndef _RAR_EXTINFO_
 #define _RAR_EXTINFO_
 
+#include "rartypes.hpp"
 
-void SetExtraInfo(CommandData *Cmd,Archive &Arc,char *Name,wchar *NameW);
-void SetExtraInfoNew(CommandData *Cmd,Archive &Arc,char *Name,wchar *NameW);
+class CommandData;
+class Archive;
+
+void SetExtraInfo(CommandData *Cmd, Archive &Arc, char *Name, wchar *NameW);
+void SetExtraInfoNew(CommandData *Cmd, Archive &Arc, char *Name, wchar *NameW);
 
 #endif
