8113c5e6e9faffb61479d7f0c5307cb748a906e1
Adaptee <adaptee@gmail.com>
2011-02-03 14:00:23 +0800

    make model.cpp stand-alone

--- coder.cpp
+++ coder.cpp
@@ -18,16 +18,6 @@ void RangeCoder::InitDecoder(Unpack *UnpackRead)
 }
 
 
-// (int) cast before "low" added only to suppress compiler warnings.
-#define ARI_DEC_NORMALIZE(code, low, range, read)                           \
-{                                                                        \
-  while ((low^(low+range))<TOP || range<BOT && ((range=-(int)low&(BOT-1)), 1)) \
-  {                                                                      \
-    code=(code << 8) | read->GetChar();                                  \
-    range <<= 8;                                                         \
-    low <<= 8;                                                           \
-  }                                                                      \
-}
 
 
 inline int RangeCoder::GetCurrentCount()
--- coder.hpp
+++ coder.hpp
@@ -29,5 +29,16 @@ class RangeCoder
     Unpack *UnpackRead;
 };
 
+// (int) cast before "low" added only to suppress compiler warnings.
+#define ARI_DEC_NORMALIZE(code, low, range, read)                           \
+{                                                                        \
+  while ((low^(low+range))<TOP || range<BOT && ((range=-(int)low&(BOT-1)), 1)) \
+  {                                                                      \
+    code=(code << 8) | read->GetChar();                                  \
+    range <<= 8;                                                         \
+    low <<= 8;                                                           \
+  }                                                                      \
+}
+
 #endif /* end of include guard: CODER_GUARD */
 
