ed0f7a9312de9d52d326dd909ec83989a11592a5
Adaptee <adaptee@gmail.com>
2011-02-03 13:02:06 +0800

    make uowners.cpp stand-alone

--- uowners.cpp
+++ uowners.cpp
@@ -1,10 +1,20 @@
+#include "archive.hpp"
+#include "filefn.hpp"
+#include "resource.hpp"
+#include "loclang.hpp"
+#include "log.hpp"
+#include "crc.hpp"
+
+#include "errhnd.hpp"
+//FIXME; duplicated info
+extern ErrorHandler ErrHandler;
 
 
-void ExtractUnixOwner(Archive &Arc,char *FileName)
+void ExtractUnixOwner(Archive &Arc, char *FileName)
 {
   if (Arc.HeaderCRC!=Arc.UOHead.HeadCRC)
   {
-    Log(Arc.FileName,St(MOwnersBroken),FileName);
+    Log(Arc.FileName, St(MOwnersBroken), FileName);
     ErrHandler.SetErrorCode(CRC_ERROR);
     return;
   }
@@ -12,7 +22,7 @@ void ExtractUnixOwner(Archive &Arc,char *FileName)
   struct passwd *pw;
   if ((pw=getpwnam(Arc.UOHead.OwnerName))==NULL)
   {
-    Log(Arc.FileName,St(MErrGetOwnerID),Arc.UOHead.OwnerName);
+    Log(Arc.FileName, St(MErrGetOwnerID), Arc.UOHead.OwnerName);
     ErrHandler.SetErrorCode(WARNING);
     return;
   }
@@ -21,38 +31,38 @@ void ExtractUnixOwner(Archive &Arc,char *FileName)
   struct group *gr;
   if ((gr=getgrnam(Arc.UOHead.GroupName))==NULL)
   {
-    Log(Arc.FileName,St(MErrGetGroupID),Arc.UOHead.GroupName);
+    Log(Arc.FileName, St(MErrGetGroupID), Arc.UOHead.GroupName);
     ErrHandler.SetErrorCode(CRC_ERROR);
     return;
   }
-  uint Attr=GetFileAttr(FileName,NULL);
+  uint Attr=GetFileAttr(FileName, NULL);
   gid_t GroupID=gr->gr_gid;
 #if defined(SAVE_LINKS) && !defined(_APPLE)
-  if (lchown(FileName,OwnerID,GroupID)!=0)
+  if (lchown(FileName, OwnerID, GroupID)!=0)
 #else
-  if (chown(FileName,OwnerID,GroupID)!=0)
+  if (chown(FileName, OwnerID, GroupID)!=0)
 #endif
   {
-    Log(Arc.FileName,St(MSetOwnersError),FileName);
+    Log(Arc.FileName, St(MSetOwnersError), FileName);
     ErrHandler.SetErrorCode(CREATE_ERROR);
   }
-  SetFileAttr(FileName,NULL,Attr);
+  SetFileAttr(FileName, NULL, Attr);
 }
 
 
-void ExtractUnixOwnerNew(Archive &Arc,char *FileName)
+void ExtractUnixOwnerNew(Archive &Arc, char *FileName)
 {
   char *OwnerName=(char *)&Arc.SubHead.SubData[0];
   int OwnerSize=strlen(OwnerName)+1;
   int GroupSize=Arc.SubHead.SubData.Size()-OwnerSize;
   char GroupName[NM];
-  strncpy(GroupName,(char *)&Arc.SubHead.SubData[OwnerSize],GroupSize);
+  strncpy(GroupName,(char *)&Arc.SubHead.SubData[OwnerSize], GroupSize);
   GroupName[GroupSize]=0;
 
   struct passwd *pw;
   if ((pw=getpwnam(OwnerName))==NULL)
   {
-    Log(Arc.FileName,St(MErrGetOwnerID),OwnerName);
+    Log(Arc.FileName, St(MErrGetOwnerID), OwnerName);
     ErrHandler.SetErrorCode(WARNING);
     return;
   }
@@ -61,20 +71,20 @@ void ExtractUnixOwnerNew(Archive &Arc,char *FileName)
   struct group *gr;
   if ((gr=getgrnam(GroupName))==NULL)
   {
-    Log(Arc.FileName,St(MErrGetGroupID),GroupName);
+    Log(Arc.FileName, St(MErrGetGroupID), GroupName);
     ErrHandler.SetErrorCode(CRC_ERROR);
     return;
   }
-  uint Attr=GetFileAttr(FileName,NULL);
+  uint Attr=GetFileAttr(FileName, NULL);
   gid_t GroupID=gr->gr_gid;
 #if defined(SAVE_LINKS) && !defined(_APPLE)
-  if (lchown(FileName,OwnerID,GroupID)!=0)
+  if (lchown(FileName, OwnerID, GroupID)!=0)
 #else
-  if (chown(FileName,OwnerID,GroupID)!=0)
+  if (chown(FileName, OwnerID, GroupID)!=0)
 #endif
   {
-    Log(Arc.FileName,St(MSetOwnersError),FileName);
+    Log(Arc.FileName, St(MSetOwnersError), FileName);
     ErrHandler.SetErrorCode(CREATE_ERROR);
   }
-  SetFileAttr(FileName,NULL,Attr);
+  SetFileAttr(FileName, NULL, Attr);
 }
